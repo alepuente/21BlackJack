62a1b6f63f0cc25b1498e8391a11c5ed2025dd67
alepuente <ale3puente@gmail.com>
2021-02-20 18:11:54 +0900

    Initial Commit Game Finished

---
+++ 21BlackJackGame.py
@@ -0,0 +1,88 @@
+import GameParts as gp
+import os
+def clear(): 
+    if(os.name == 'nt'):
+        os.system('cls')
+    else:
+        os.system('clear')
+
+loses = 0
+wins = 0
+ties = 0
+def StartGame():   
+    clear()
+    global wins 
+    global loses
+    global ties
+    #Game variables
+    gameState = 1
+    deck = gp.Deck()
+    player1 = gp.Hand(0)
+    dealer = gp.Hand(1)
+
+    #Before starting the game shuffle the cards
+    deck.suffleCards()
+
+    #Game starts with 2 cards each and one dealer card unknown 
+    for deal in range(2):
+        player1.hit(deck)
+        dealer.hit(deck)    
+    #Shows initial hands
+    print('Dealer Hand is:\n{}\n# #\n'.format(dealer.cards[-1].showCard()))
+    print('Player hand:')
+    print(player1.showCards())
+    #Check if there is any aces in the hand
+    player1.checkAces()    
+    print('Player hand Value: {}\n'.format(player1.handValue))
+    #Lets the player decide if hit or stay
+    while(gameState):
+        choice = input('Select an option 0: Stay | 1:Hit\n')
+        clear()   
+        if(choice == '1'):
+            player1.hit(deck)
+            print('Player hand:')
+            print(player1.showCards())
+            print('Player hand value:{}\n'.format(player1.handValue))
+            if(player1.checkAces()):
+                print('Player hand value:{}\n'.format(player1.handValue))
+            if(player1.handValue>21):#Check if player did not bust
+                gameState = 0
+            
+        else:#If stay the dealer hits until more than 17 
+            while(dealer.handValue < 17):
+                dealer.hit(deck)
+                print('Dealer hand:')
+                print( dealer.showCards())
+                print('Dealer hand value is: {}\n'.format(dealer.handValue))
+            gameState = 0
+        
+    #Game result is calculated
+    if(player1.handValue>21):
+        print("You busted with {} you lose".format(player1.handValue))
+        loses += 1
+    elif(player1.handValue == 21 and dealer.handValue == 21):
+        print("Both players have 21, its a tied game")
+        ties += 1
+    elif(dealer.handValue>21 and player1.handValue<=21):
+        print("Dealer busted with {} you win".format(dealer.handValue))
+        wins += 1
+    elif(dealer.handValue <= 21 and player1.handValue <= dealer.handValue):
+        print("Dealer score({}) is same or higher than yours({}) you lose".format(dealer.handValue,player1.handValue))
+        loses += 1
+    elif(dealer.handValue < 21 and player1.handValue>= dealer.handValue):
+        print("Your score({}) is higher than dealers({}) you win".format(player1.handValue,dealer.handValue))
+        wins += 1
+
+game = 1
+
+while(game):
+    StartGame()
+    print('-----------Score----------------\nWins:{}\nLoses:{}\nTies:{}\n'.format(wins,loses,ties))
+    game = int(input("Want to play again??? 0:No 1:Yes \n"))
+    clear()
+
+
+    
+
+
+
---
+++ GameParts.py
@@ -0,0 +1,77 @@
+import random as r
+
+class Deck():
+    cardsValues = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+    cardsSuits = ['♠','♥','♦','♣']
+    cards = None
+    def __init__(self):#Create deck with suits and values
+        self.cards = []
+        for cardSuit in self.cardsSuits:
+            for cardValue in self.cardsValues:
+                self.cards.append(Card(cardValue, cardSuit))
+
+    def suffleCards(self):
+        r.shuffle(self.cards)
+    def takeCard(self):
+        return self.cards.pop(0)
+
+
+class Card():
+    value = None
+    suit = None
+    def __init__(self, cardValue, cardSuit):
+        self.value = cardValue
+        self.suit = cardSuit
+
+    def showCard(self):
+        return self.suit + ' ' + self.value
+
+
+class Hand():
+    isDealer = None
+    cards = []
+    acesCount = None
+    handValue = None
+    def __init__(self,isDealer):
+        self.isDealer = isDealer
+        self.cards = []
+        self.acesCount = 0
+        self.handValue = 0
+
+    def hit(self,deck):#Get a card from the top of the deck and adds it to the hand value
+        self.cards.append(deck.takeCard())
+        card = self.cards[-1]
+        if(card.value.isnumeric()):
+            self.handValue += int(card.value)
+        elif(card.value == "A"):
+            if(not self.isDealer):
+                self.acesCount += 1
+            elif((self.handValue+11) <= 21):
+                self.handValue += 11
+            else:
+                self.handValue += 1
+        else:
+            self.handValue += 10
+
+    def showCards(self):
+        cardsString = ''
+        for card in self.cards:
+            cardsString += '\n'+card.showCard()
+        return cardsString
+
+
+    #Checks how many ace cards the player got and lets the player choose the value
+    def checkAces(self):
+        if(self.acesCount > 0):
+            for ace in range(self.acesCount):
+                choice = input('Choose the value for your Ace (Enter 1 or 11): \n')
+                if(choice == '1'):
+                    self.handValue +=1
+                else:
+                    self.handValue +=11
+        self.acesCount = 0
+        return True
+        
+    
+        
+
